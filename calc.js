5ff6df131081dbab2272189d953100d8ee897deb
IsmaSb <100977822+IsmaSb@users.noreply.github.com>
2022-09-22 21:58:51 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,21 @@
+function Operation(operand) { 
+				
+
+    var firstValue = parseInt(document.getElementById('primerNumero').value);
+    var secondValue = parseInt(document.getElementById('segundoNumero').value);				
+    var result = 0;
+    switch(operand){
+
+        case '+' : 	result = ( (x, y) => {	return x + y; } )( firstValue, secondValue );
+                    break;
+
+        case '-' : 	result = ( (x, y) => {	return x - y; } )( firstValue, secondValue );
+                    break;
+        case '*' : 	result = ( (x, y) => {	return x * y; } )( firstValue, secondValue );
+                    break;
+        case '/' : 	result = ( (x, y) => {	return x / y; } )( firstValue, secondValue );
+                    break;
+    }
+    
+    alert(result);
+}
